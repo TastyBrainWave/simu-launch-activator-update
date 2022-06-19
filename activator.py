d66040a5d9a820f469bf529cb1f12c23fb5db4ee
Andy Woods <andytwoods@users.noreply.github.com>
2022-06-19 11:23:31 +0100

    activator

---
+++ activator.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+ip_address = os.popen("adb shell ip route").read()
+print(ip_address)
+ip_address = ip_address.split(" ")[8]
+print(ip_address)
+
+os.system("adb tcpip 5555")
+
+payload = {
+    'remote_address': ip_address
+}
+
+success = False
+
+response = requests.post("http://192.168.1.100:8000/connect", data=payload)
+print(response)
+success = response.json()["success"]
+
+print("Finished")
