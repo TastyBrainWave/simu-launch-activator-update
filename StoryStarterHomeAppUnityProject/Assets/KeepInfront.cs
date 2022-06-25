a7f1908c7bdc2ecc889a226075ff989e37a6e0ba
TrajectoryRob <104428934+TrajectoryRob@users.noreply.github.com>
2022-06-25 21:03:12 +0100

    Updates to home app
    Lerp added to banner and APK added to repo

--- StoryStarterHomeAppUnityProject/Assets/KeepInfront.cs
+++ StoryStarterHomeAppUnityProject/Assets/KeepInfront.cs
@@ -5,7 +5,9 @@ using UnityEditor.XR.LegacyInputHelpers;
 
 public class KeepInfront : MonoBehaviour
 {
+    public float speed;
     private float offset;
+    private Quaternion target;
 
     private void Start()
     {
@@ -15,7 +17,11 @@ public class KeepInfront : MonoBehaviour
     private void Update()
     {
         gameObject.transform.position = new Vector3(Camera.main.transform.position.x, 1.3f, Camera.main.transform.position.z);
-        gameObject.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        Vector3 targetY = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        target = Quaternion.Euler(targetY);
+        gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, target, speed * Time.deltaTime);
+
+       
     }
 
 }
