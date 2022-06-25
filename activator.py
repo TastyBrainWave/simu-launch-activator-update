412694be05f16b9aa87da3d44c07f3ba70df118d
Andy Woods <andytwoods@users.noreply.github.com>
2022-06-25 13:12:31 +0100

    activator without wifi

--- activator.py
+++ activator.py
@@ -1,21 +1,25 @@
 import requests
 import os
+import time
+import datetime
+while True:
+    time_only = datetime.datetime.now().strftime("%H:%M:%S")
+    ip_address = os.popen("adb shell ip route").read()
+    print(f"{time_only} { ip_address}")
+    if ip_address:
+        ip_address = ip_address.split(" ")[8]
+        print(ip_address)
 
-ip_address = os.popen("adb shell ip route").read()
-print(ip_address)
-ip_address = ip_address.split(" ")[8]
-print(ip_address)
+        os.system("adb tcpip 5555")
 
-os.system("adb tcpip 5555")
+        payload = {
+            'remote_address': ip_address
+        }
+        print('restarting in tcpip mode... waiting 3 seconds')
+        time.sleep(3)
+        print('letting storystarter know about Quest...')
+        outcome = os.popen("""adb shell "printf 'GET /connect HTTP/1.1\r\n http://192.168.0.155 \r\n\r\n' | nc 192.168.0.155 8000""").read()
+        print(outcome,222)
 
-payload = {
-    'remote_address': ip_address
-}
-
-success = False
-
-response = requests.post("http://192.168.1.100:8000/connect", data=payload)
-print(response)
-success = response.json()["success"]
-
-print("Finished")
+        print("Finished")
+    time.sleep(5)
